a9a4b10f040c14e16d95ac53dc37d632bd19ba35
Boluwatife Emmanuel Collins <bolucomputercollins@outlook.com>
2022-12-30 01:09:17 -0800

    Add files via upload

---
+++ CITRUS COLLEGE/C#/Sales Price Calculator/Sales Price Calculator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales_Price_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declarations
+                double OriginalPrice, discountPercentage, discountPrice, salesPrice;
+
+                //Get User Imput
+                OriginalPrice = double.Parse(txtPriceOriginal.Text);
+                discountPercentage = double.Parse(txtSalesPercentage.Text);
+
+                //Calculate
+                discountPrice = (discountPercentage / 100) * OriginalPrice;
+                salesPrice = OriginalPrice - discountPrice;
+
+                //Output
+                lblResult.Text = salesPrice.ToString("c");
+            }
+            catch
+            {
+                MessageBox.Show("Enter a number");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CITRUS COLLEGE/C#/Sales Total (Read from file)/Sales Total (Read from file)/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sales_Total__Read_from_file_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+          try
+            {
+                //Declarations
+                decimal sales;
+                decimal total = 0m;
+                StreamReader inputFile;
+
+
+                //File Input
+
+                inputFile = File.OpenText(@"C:\Users\colli\iCloudDrive\EDUCATION\UNIVERSITY\CITRUS COLLEGE\COMPUTER SCIENCE 111\PROJECT FOLDER\Sales.txt");
+
+                //Calculate
+                while (!inputFile.EndOfStream)
+                {
+                    sales = decimal.Parse(inputFile.ReadLine());
+                    total += sales;
+                }
+                inputFile.Close();
+
+                // Output
+                lblResult.Text = total.ToString("C");
+            }   
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
