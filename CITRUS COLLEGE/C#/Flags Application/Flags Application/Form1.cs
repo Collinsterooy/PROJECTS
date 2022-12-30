bdd96495cfa06c3f1594742b5c78a2aaa5b5d05f
Boluwatife Emmanuel Collins <bolucomputercollins@outlook.com>
2022-12-30 00:59:26 -0800

    Add files via upload

---
+++ CITRUS COLLEGE/C#/Flags Application/Flags Application/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flags_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pctrbFinland_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "Finland";
+        }
+
+        private void pctrbFrance_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "France";
+        }
+
+        private void pctrbGermany_Click(object sender, EventArgs e)
+        {
+            lblCountry.Text = "Germany";
+        }
+    }
+}
