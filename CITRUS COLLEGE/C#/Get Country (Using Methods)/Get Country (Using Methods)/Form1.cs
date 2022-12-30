b355d128105a8a695e4874543edb49f285e6fd98
Boluwatife Emmanuel Collins <bolucomputercollins@outlook.com>
2022-12-30 01:05:11 -0800

    Add files via upload

---
+++ CITRUS COLLEGE/C#/Get Country (Using Methods)/Get Country (Using Methods)/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Get_Country__Using_Methods_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Method Declaration
+
+        private void GetFileName(out string SelectedFile)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                SelectedFile = openFile.FileName;
+            }
+            else
+            {
+                SelectedFile = null;
+            }
+        }
+
+       private void GetCountry(string filename)
+        {
+            try
+            {
+
+                //Declarations
+                string counrtyName;
+
+                StreamReader inputfile;
+
+                inputfile = File.OpenText(filename);
+
+                lstbxCountries.Items.Clear();
+
+                while (!inputfile.EndOfStream)
+                {
+                    counrtyName = inputfile.ReadLine();
+
+                    lstbxCountries.Items.Add(counrtyName);
+                }
+                inputfile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnGetCntry_Click(object sender, EventArgs e)
+        {
+            string filename;
+
+            GetFileName(out filename);
+            GetCountry(filename);
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
