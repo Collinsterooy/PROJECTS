1091869d697e48f3a896e26ae9892f554ddaf31d
Boluwatife Emmanuel Collins <bolucomputercollins@outlook.com>
2022-12-30 01:06:36 -0800

    Add files via upload

---
+++ CITRUS COLLEGE/C#/NumericToWord/NumericToWord/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumericToWord
+{
+    public partial class NumericToWord : Form
+    {
+        public NumericToWord()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            lblFive.Text = "Five";
+            lblFive.Visible = true;
+            lblNine.Visible = false;
+            lblThree.Visible = false;
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            lblNine.Text = "Nine";
+            lblNine.Visible = true;
+            lblFive.Visible = false;
+            lblThree.Visible=false;
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            lblThree.Text = "Three";
+            lblThree.Visible = true;
+            lblFive.Visible = false;
+            lblNine.Visible = false;
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
