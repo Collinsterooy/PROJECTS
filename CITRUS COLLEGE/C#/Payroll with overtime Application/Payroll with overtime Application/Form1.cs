b62a9b015e704149c99156ae1f3f45af1593ff82
Boluwatife Emmanuel Collins <bolucomputercollins@outlook.com>
2022-12-30 01:08:20 -0800

    Add files via upload

---
+++ CITRUS COLLEGE/C#/Payroll with overtime Application/Payroll with overtime Application/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_overtime_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcGrossPay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Declaration
+
+                // Constants
+                const decimal BASE_HOURS = 40m, OVERTIME_RATE = 1.5m;
+
+                //local Variables
+                decimal hoursWorked, hourlyRate, basePay, overtimePay, overtimeHours, grossPay;
+
+
+                //User Input
+                hoursWorked = decimal.Parse(txtHoursWorked.Text);
+                hourlyRate = decimal.Parse(txtHourlyRate.Text);
+
+                //Calculations
+                
+                if (hoursWorked > BASE_HOURS)
+                {
+                    basePay = BASE_HOURS * hourlyRate;
+                    overtimeHours = hoursWorked - BASE_HOURS;
+                    overtimePay = overtimeHours * (hourlyRate * OVERTIME_RATE);
+                    grossPay = overtimePay + basePay;
+                }
+                else
+                {
+                    grossPay = hoursWorked * hourlyRate;
+                }
+
+                //Output
+                lblResult.Text = grossPay.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHoursWorked.Text = "";
+            txtHourlyRate.Text = "";
+            lblResult.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CITRUS COLLEGE/C#/Quiz worksheet/Quiz worksheet/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_worksheet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click_Click(object sender, EventArgs e)
+        {
+            int finalValue;
+            int row = 3;
+            int col = 4;
+
+            finalValue = myFunc(row, col);
+            llblResult.Text = Convert.ToString(finalValue);
+        }
+        int myFunc(int rowPar, int colPar)
+        {
+            int counter = 1;
+            int tempCol;
+
+            tempCol = colPar;
+
+            while (rowPar >= 0)
+            {
+                while (colPar >= 0)
+                {
+                    counter = counter + 1;
+                    colPar = colPar - 1;
+                }
+                colPar = tempCol;
+                rowPar = rowPar - 1;
+            }
+
+            return counter;
+
+        }
+    }
+}
+               
+
+            
+
